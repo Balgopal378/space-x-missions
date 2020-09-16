af16ce0a6715920deaef184fbed174e723763e55
Balgopal Prusty <prusty.vicky@gmail.com>
2020-09-16 11:18:39 +0530

    add chip component

---
+++ src/app/chip/chip.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-chip',
+  templateUrl: './chip.component.html',
+  styleUrls: ['./chip.component.css']
+})
+export class ChipComponent implements OnInit {
+    @Input() option: {[key: string]: string};
+    @Output() yearFilterChanged: EventEmitter<{[key: string]: string}> = new EventEmitter(null);
+    @Output() launchFilterChanged: EventEmitter<{[key: string]: string}> = new EventEmitter(null);
+    @Output() landingFilterChanged: EventEmitter<{[key: string]: string}> = new EventEmitter(null);
+
+    isClicked = false;
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    onClick(val: {[key: string]: string}) {
+        // this.isClicked = !this.isClicked;
+        if (val.name === 'year') {
+            this.yearFilterChanged.emit(val);
+        }
+        if (val.name === 'launch') {
+            this.launchFilterChanged.emit(val);
+        }
+        if (val.name === 'land') {
+            this.landingFilterChanged.emit(val);
+        }
+    }
+
+}
