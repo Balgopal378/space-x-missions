edfff4d7d7a046038b9faf13ffbc4471cb3936f1
Balgopal Prusty <prusty.vicky@gmail.com>
2020-09-16 11:18:24 +0530

    add filter component

---
+++ src/app/filters/filters.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { LAUNCH_YEAR_FILTERS, SUCCESSFULL_LANDING_FILTERS, SUCCESSFULL_LAUNCH_FILTERS } from '../constants/filters.constant';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.css']
+})
+export class FiltersComponent implements OnInit {
+    @Output() yearFilterChanged: EventEmitter<{[key: string]: string}> = new EventEmitter(null);
+    @Output() launchFilterChanged: EventEmitter<{[key: string]: string}> = new EventEmitter(null);
+    @Output() landingFilterChanged: EventEmitter<{[key: string]: string}> = new EventEmitter(null);
+    previousYearElement: HTMLElement;
+    previousLaunchElement: HTMLElement;
+    previousLandElement: HTMLElement;
+    yearFilterOptions = LAUNCH_YEAR_FILTERS;
+    launchFilterOptions = SUCCESSFULL_LAUNCH_FILTERS;
+    landingFilterOptions = SUCCESSFULL_LANDING_FILTERS;
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    onYearFilterChange(val: {[key: string]: string}) {
+        this.yearFilterChanged.emit(val);
+    }
+
+    onLaunchFilterChange(val: {[key: string]: string}) {
+        this.launchFilterChanged.emit(val);
+    }
+
+    onLandingFilterChange(val: {[key: string]: string}) {
+        this.landingFilterChanged.emit(val);
+    }
+
+    onYearFilterOptionsClick(event) {
+        const clickedElement = event.target || event.srcElement;
+        if ( clickedElement.nodeName === 'BUTTON' ) {
+            if (this.previousYearElement) {
+                this.previousYearElement.classList.remove('active');
+            }
+            const isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector('.active');
+            // if a Button already has Class: .active
+            if ( isCertainButtonAlreadyActive ) {
+                isCertainButtonAlreadyActive.classList.remove('active');
+            } else {
+                clickedElement.className += ' active';
+                this.previousYearElement = clickedElement;
+            }
+        }
+
+    }
+
+    onLaunchFilterOptionsClick(event) {
+        const clickedElement = event.target || event.srcElement;
+        if ( clickedElement.nodeName === 'BUTTON' ) {
+            if (this.previousLaunchElement) {
+                this.previousLaunchElement.classList.remove('active');
+            }
+            const isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector('.active');
+            // if a Button already has Class: .active
+            if ( isCertainButtonAlreadyActive ) {
+                isCertainButtonAlreadyActive.classList.remove('active');
+            } else {
+                clickedElement.className += ' active';
+                this.previousLaunchElement = clickedElement;
+            }
+        }
+    }
+
+    onLandFilterOptionsClick(event) {
+        const clickedElement = event.target || event.srcElement;
+        if ( clickedElement.nodeName === 'BUTTON' ) {
+            if (this.previousLandElement) {
+                this.previousLandElement.classList.remove('active');
+            }
+            const isCertainButtonAlreadyActive = clickedElement.parentElement.querySelector('.active');
+            // if a Button already has Class: .active
+            if ( isCertainButtonAlreadyActive ) {
+                isCertainButtonAlreadyActive.classList.remove('active');
+            } else {
+                clickedElement.className += ' active';
+                this.previousLandElement = clickedElement;
+            }
+        }
+    }
+}
