ea1801394db371b28256c8ea76a72faed230db25
Balgopal Prusty <prusty.vicky@gmail.com>
2020-09-16 11:12:42 +0530

    add environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    apiUrl: 'https://api.spacexdata.com/v3/launches',
+},
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  config: {
+    apiUrl: 'https://api.spacexdata.com/v3/launches',
+},
 };
 
 /*
